40e712d627e29dca382f6229a24ab167682861a7
mesatyamsharma123 <96259558+mesatyamsharma123@users.noreply.github.com>
2025-04-01 17:03:33 +0530

    done2

--- back/src/component/Home.jsx
+++ back/src/component/Home.jsx
@@ -4,7 +4,7 @@ import pic2 from '../../public/pic2.jpg'
 function Home() {
   return (
     <div className=' flex flex-col items-center justify-center md:items-center md:justify-center h-screen w-screen bg-cover' style={{backgroundImage:`url(${pic})`}}>
- <div className='w-xs md:w-xl  md:w-sm bg-white rounded-2xl bg-cover h-200 text-white' style={{backgroundImage:`url(${pic2})`}} >
+ <div className='w-xs md:w-xl  md:w-sm bg-white rounded-2xl bg-cover h-200 md:200 text-white' style={{backgroundImage:`url(${pic2})`}} >
 jb,b.bk.bk.bk,bb.b.
 mhvhvvjvjvlbvklbklblb
  </div>
