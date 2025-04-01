c0b63a2b4b8e642bfd6c4b338ce89c628cfbb8b5
mesatyamsharma123 <96259558+mesatyamsharma123@users.noreply.github.com>
2025-04-01 16:49:41 +0530

    push

--- back/src/component/Home.jsx
+++ back/src/component/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import pic from '../../public/pic.avif'
+import pic2 from '../../public/pic2.jpg'
 function Home() {
   return (
-    <div className='h-screen bg-cover' style={{backgroundImage:`url(${pic})`}}>
-      ,m,
+    <div className=' flex flex-col items-center justify-center h-screen w-screen bg-cover' style={{backgroundImage:`url(${pic})`}}>
+ <div className='w-xs md:w-sm bg-white rounded-2xl bg-cover h-200' style={{backgroundImage:`url(${pic2})`}} >
+cvv
+ </div>
     </div>
   )
 }
