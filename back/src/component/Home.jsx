867aad1047e323223fdf8fc40c1abb8e82724c46
mesatyamsharma123 <96259558+mesatyamsharma123@users.noreply.github.com>
2025-04-01 16:24:11 +0530

    done

---
+++ back/src/component/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import pic from '../../public/pic.avif'
+function Home() {
+  return (
+    <div className='h-screen bg-cover' style={{backgroundImage:`url(${pic})`}}>
+      ,m,
+    </div>
+  )
+}
+
+export default Home
